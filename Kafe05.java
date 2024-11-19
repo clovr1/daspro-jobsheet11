77ee103eec51284974c224ab7f6433bc28dd2877
clovr1 <keenanaryasatya01@gmail.com>
2024-11-19 13:27:30 +0700

    Create Kafe05.java

---
+++ Kafe05.java
@@ -0,0 +1,18 @@
+public class Kafe05 {
+    public static void Menu() {
+        System.out.println("====MENU KAFE RESTO====");
+        System.out.println("1. Kopi Ireng - Rp10.000");
+        System.out.println("2. Cappucino - Rp20.000");
+        System.out.println("3. Latte - Rp15.000");
+        System.out.println("4. Teh Tarik - Rp12.000" );
+        System.out.println("5. Roti Bakar - Rp5.000");
+        System.out.println("6. Mie Goreng - Rp5.000");
+        System.out.println("============");
+        System.out.println("Silahkan pilih menu yang Anda inginkan");
+        
+    }
+        public static void main(String[] args) {
+            Menu();
+        }
+    }
+
